4d3580cc0eadc23c3a3b82136e9859bbf7d76b83
Sharmin64 <lizaeatsbanana@gmail.com>
2023-02-15 19:59:13 +0600

    js file added

--- js_folder/cart.js
+++ js_folder/cart.js
@@ -11,18 +11,21 @@ function updateCaseNumber(isIncrease) {
   caseNumberField.value = newCaseNumber;
   return newCaseNumber;
 }
-
-document.getElementById("btn-case-plus").addEventListener("click", function () {
-  const newCaseNumber = updateCaseNumber(true);
+function updateCaseTotalPrice(newCaseNumber) {
   const caseTotalPrice = newCaseNumber * 59;
   const caseTotalElement = document.getElementById("case-total");
   caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
+  updateCaseTotalPrice(newCaseNumber);
+  calculateSubTotal();
 });
 document
   .getElementById("btn-case-minus")
   .addEventListener("click", function () {
     const newCaseNumber = updateCaseNumber(false);
-    const caseTotalPrice = newCaseNumber * 59;
-    const caseTotalElement = document.getElementById("case-total");
-    caseTotalElement.innerText = caseTotalPrice;
+    updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
   });
